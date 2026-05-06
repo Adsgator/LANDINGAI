b5f0ca5e802110ae2c88010a42f50c4565a964d7
AdsGator Chief Engineer <agenciaadsgator@gmail.com>
2026-05-06 03:37:23 -0300

    LandingAI - v2.0.1 arrumado .gitignore

--- scratch/list_ids.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const js = fs.readFileSync('assets/app.js', 'utf8');
-
-// Extract all getElementById calls
-const matches = [...js.matchAll(/getElementById\(['"]([^'"]+)['"]\)/g)];
-const ids = [...new Set(matches.map(m => m[1]))].sort();
-console.log('All getElementById IDs used in app.js:');
-ids.forEach(id => console.log(' -', id));
